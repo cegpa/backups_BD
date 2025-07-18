f3b0f957737951d2dd40c4d53f5c554d22d332b1
unknown <edu.estudos.dev@gmail.com>
2025-07-18 11:40:17 -0300

    Backup autom├ítico em 18/07/2025 11:40:17,31

--- backup_cegpa.bat
+++ backup_cegpa.bat
@@ -2,7 +2,7 @@
 REM Configurações
 set DATA=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%
 set HORA=%TIME:~0,2%
-set BACKUP_DIR=C:\Sistema-cegpa
+set BACKUP_DIR=C:\Sistema-cegpa\backups_BD
 set MYSQL_BIN=C:\xampp\mysql\bin
 set MYSQL_USER=root
 set MYSQL_DB=cegpa
@@ -13,11 +13,13 @@ if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
 REM Faz o backup
 "%MYSQL_BIN%\mysqldump.exe" -u %MYSQL_USER% %MYSQL_DB% > "%BACKUP_DIR%\cegpa_%DATA%_%HORA%.sql"
 
-REM Remove arquivos .sql com mais de 7 dias
-forfiles /p "%BACKUP_DIR%" /m *.sql /d -7 /c "cmd /c del @path"
+REM Mantém apenas os 7 arquivos .sql mais recentes, apaga os mais antigos
+pushd "%BACKUP_DIR%"
+for /f "skip=7 delims=" %%F in ('dir /b /a-d /o-d *.sql') do del "%%F"
+popd
 
 REM Comandos do Git com log
-cd "%BACKUP_DIR%"
+cd /d "%BACKUP_DIR%"
 echo %DATE% %TIME% >> backup_log.txt
 git add . >> backup_log.txt 2>&1
 git commit -m "Backup automático em %DATE% %TIME%" >> backup_log.txt 2>&1
