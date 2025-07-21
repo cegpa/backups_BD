44099be527f5c6649cdd526021609bc84a6a5e38
unknown <edu.estudos.dev@gmail.com>
2025-07-21 09:29:29 -0300

    Backup autom├ítico em 21/07/2025  9:29:29,16

--- backup_cegpa.bat
+++ backup_cegpa.bat
@@ -11,7 +11,7 @@ REM Cria a pasta de backup se não existir
 if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
 
 REM Faz o backup
-"%MYSQL_BIN%\mysqldump.exe" -u %MYSQL_USER% %MYSQL_DB% > "%BACKUP_DIR%\cegpa_%DATA%_%HORA%.sql"
+"%MYSQL_BIN%\mysqldump.exe" -u %MYSQL_USER% %MYSQL_DB% > "%BACKUP_DIR%\\cegpa_%DATA%_%HORA%.sql"
 
 REM Mantém apenas os 7 arquivos .sql mais recentes, apaga os mais antigos
 pushd "%BACKUP_DIR%"
@@ -24,3 +24,6 @@ echo %DATE% %TIME% >> backup_log.txt
 git add . >> backup_log.txt 2>&1
 git commit -m "Backup automático em %DATE% %TIME%" >> backup_log.txt 2>&1
 git push origin main >> backup_log.txt 2>&1
+
+REM Copia a pasta de backup para o Google Drive
+xcopy "C:\\Sistema-cegpa\\backups_BD" "G:\\Meu Drive\\backups_BD" /E /I /Y
